e3fc6f945dd4eba6368f35bc227e7176d74506d5
David Mongeau-Petitpas <dmp@atelierfolklore.ca>
2015-04-08 11:23:18 -0400

    Updates

--- php/path.zsh
+++ php/path.zsh
@@ -1,2 +1,3 @@
-export PHP_PATH="/Applications/MAMP/bin/php/php5.5.18"
-export PATH="$PHP_PATH/bin:$PATH"
+export PHP_PATH="/Applications/MAMP/bin/php/php5.6.7"
+export COMPOSER_BIN_PATH="/Users/dmp/.composer/vendor/bin"
+export PATH="$PHP_PATH/bin:$COMPOSER_BIN_PATH:$PATH"
