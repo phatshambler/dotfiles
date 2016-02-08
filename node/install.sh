b08bcf73e3d3a12516a20ad2e937eb037a8cdbf5
David Mongeau-Petitpas <dmp@atelierfolklore.ca>
2016-02-08 11:06:59 -0500

    Better

--- node/install.sh
+++ node/install.sh
@@ -8,18 +8,23 @@ fi
 source $(brew --prefix nvm)/nvm.sh
 
 nvm install v0.12.7
-nvm install v4.1.1
+nvm install v4.2.4
+nvm install v5.4.0
 
 nvm use v0.12.7
 npm update -g npm
 npm install -g bower yo gulp grunt-cli
 
-nvm use v4.1.1
+nvm use v4.2.4
 npm update -g npm
 npm install -g bower yo gulp grunt-cli
 
-nvm use v0.12.7
-nvm alias default v0.12.7
+nvm use v5.4.0
+npm update -g npm
+npm install -g bower yo gulp grunt-cli
+
+nvm use v4.2.4
+nvm alias default v4.2.4
 
 
 exit 0
