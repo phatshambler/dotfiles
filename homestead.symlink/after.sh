6c7c1bfe48a96aca9d12ba668bd2acce92aedff9
David Mongeau-Petitpas <dmp@atelierfolklore.ca>
2016-11-01 10:27:50 -0400

    Added homestead

--- homestead.symlink/after.sh
+++ homestead.symlink/after.sh
@@ -3,3 +3,11 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+php /home/vagrant/Code/ffframe.com/artisan migrate
+
+php /home/vagrant/Code/manivelle/backend/artisan migrate
+
+php /home/vagrant/Code/telecommande/artisan migrate
+
+php /home/vagrant/Code/archives.nte.qc.ca/artisan migrate
