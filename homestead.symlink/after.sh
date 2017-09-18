599632b878450217c24e439e9eb20055138d01b9
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2017-09-18 17:55:42 -0400

    Ignore Homestead configuration

--- homestead.symlink/after.sh
+++ homestead.symlink/after.sh
@@ -8,16 +8,6 @@ sudo add-apt-repository ppa:chris-needham/ppa
 sudo apt-get update
 sudo apt-get -y install chromium-browser xvfb pulseaudio ffmpeg libfdk-aac-dev php-mcrypt audiowaveform
 
-php /home/vagrant/Code/ffframe.com/artisan migrate
-
-php /home/vagrant/Code/manivelle/backend/artisan migrate
-
-php /home/vagrant/Code/telecommande/artisan migrate
-
-php /home/vagrant/Code/archives.nte.qc.ca/artisan migrate
-
-php /home/vagrant/Code/landandlegends.com/artisan migrate
-
 sudo locale-gen fr_CA
 sudo locale-gen fr_CA.UTF-8
 sudo update-locale
