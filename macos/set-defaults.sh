6a6c46dcd971f1b06fb18f75c92ea49e8b23d2f7
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2017-10-31 15:03:04 -0400

    Fix with high sierra

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -622,7 +622,7 @@ defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # Disable local Time Machine backups
-hash tmutil &> /dev/null && sudo tmutil disablelocal
+# hash tmutil &> /dev/null && sudo tmutil disablelocal
 
 ###############################################################################
 # Activity Monitor                                                            #
