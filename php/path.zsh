97f781100af4f391f2aedd57ad3533fb7ec5f802
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-01-03 21:56:49 -0500

    Updated php path

--- php/path.zsh
+++ php/path.zsh
@@ -1,3 +1,3 @@
 export COMPOSER_BIN_PATH="/Users/dmp/.composer/vendor/bin"
-export PHP_PATH="$(brew --prefix homebrew/php/php70)/bin"
+export PHP_PATH="$(brew --prefix php71)/bin"
 export PATH="$COMPOSER_BIN_PATH:$PHP_PATH:$PATH"
